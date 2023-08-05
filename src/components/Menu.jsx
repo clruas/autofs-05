b76e228b428c23c4e5037e0fb9a33ed6ea1ed49b
Cleverson <clruas@gmail.com>
2023-08-05 01:42:38 -0300

    Configuracao do menu desktop

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,43 @@
 import MenuItem from "./MenuItem"
+import { AiOutlineDashboard } from 'react-icons/ai'
+import { PiStudent, PiChalkboardTeacher, PiBooks, PiCurrencyCircleDollar, PiUsers, PiGauge } from 'react-icons/pi'
 
 function Menu() {
 	return (
-		<nav className="md:grid md:grid-flow-row">
-			<MenuItem to="/">Home</MenuItem>
-			<MenuItem to="/alunos">Alunos</MenuItem>
-			<MenuItem to="/instrutores">Instrutores</MenuItem>
-			<MenuItem to="/aulas">Aulas</MenuItem>
-			<MenuItem to="/financeiro">Financeiro</MenuItem>
-			<MenuItem to="/usuarios">Usuarios</MenuItem>
+		<nav className="grid md:grid-flow-row md:px-4">
+			<MenuItem to="/" label="Home" image={PiGauge} />
+			<MenuItem to="/alunos" label="Alunos" image={PiStudent} />
+			<MenuItem to="/instrutores" label="Instrutores" image={PiChalkboardTeacher} />
+			<MenuItem to="/aulas" label="Aulas" image={PiBooks} />
+			<MenuItem to="/financeiro" label="Financeiro" image={PiCurrencyCircleDollar} />
+			<MenuItem to="/usuarios" label="Usuários" image={PiUsers} />
+
+			{/* 
+			<MenuItem to="/">
+				<PiGauge size={20} />
+				<span>Home</span>
+			</MenuItem>
+			<MenuItem to="/alunos">
+				<PiStudent size={24}/>
+				<span>Alunos</span>
+			</MenuItem>
+			<MenuItem to="/instrutores">
+				<PiChalkboardTeacher size={24}/>
+				<span>Instrutores</span>
+			</MenuItem>
+			<MenuItem to="/aulas">
+				<PiBooks size={24}/>
+				<span>Aulas</span>
+			</MenuItem>
+			<MenuItem to="/financeiro">
+				<PiCurrencyCircleDollar size={24}/>
+				<span>Financeiro</span>
+			</MenuItem>
+			<MenuItem to="/usuarios">
+				<PiUsers size={24}/>
+				<span>Usuarios</span>
+			</MenuItem>
+			*/}
 		</nav>
 	)
 }
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,18 @@
 import { NavLink } from "react-router-dom"
 
-function MenuItem({ children, ...rest}){
-	return <NavLink className="" {...rest}>{children}</NavLink>
+function MenuItem({ image: Component, label, ...rest}){
+	return <NavLink className="grid md:grid-cols-[32px_1fr] md:p-2 md:rounded" {...rest}>
+		<Component size={24} />
+		<span>{label}</span>
+	</NavLink>
 }
+
+/*
+function MenuItem({ children, ...rest}){
+	return <NavLink className="grid md:grid-cols-2 md:rounded" {...rest}>
+	{children}
+	</NavLink>
+}
+*/
 
 export default MenuItem
