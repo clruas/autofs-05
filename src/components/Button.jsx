1a82255635ff742261579e248c21d497eb51e6b7
Cleverson <clruas@gmail.com>
2023-08-08 23:32:58 -0300

    Concluindo o componente Button em arquivo separado

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,29 @@
+import { Link } from 'react-router-dom'
 import { twMerge } from 'tailwind-merge'
 
-function Button({ children, ...rest }){
-    return (
-        <button 
-            {...rest}
-            className={`${twMerge('grid grid-flow-col-dense items-center border bg-sky-500 text-white rounded text-xs py-2 px-3 hover:bg-sky-600', rest.className)}`}
-        >{children}</button>
-    )
+function Button({ to, className, children, variant="normal", ...rest }){
+	const variants = {
+		normal: "bg-slate-300 border-slate-300 text-slate-600 hover:bg-slate-400",
+		primary: "bg-sky-500 border-sky-500 text-white",
+		outline: "border-slate-400",
+		success: "bg-emerald-500 border-emerald-500 text-white"
+	}
+	const classes = twMerge("border grid grid-flow-col-dense items-center rounded text-sm font-semibold py-2 px-2 outline-none", variants[variant], className)
+	//const classes = twMerge("grid grid-flow-col-dense items-center border bg-sky-500 text-white rounded text-xs py-2 px-3 hover:bg-sky-600", className)
+	if(to){
+		return <Link className={classes} to={to} {...rest}>{children}</Link>
+	} else {
+		return <button className={classes} {...rest}>{children}</button>
+	}
 }
+
+// function Button({ children, ...rest }){
+//     return (
+//         <button 
+//             {...rest}
+//             className={`${twMerge('grid grid-flow-col-dense items-center border bg-sky-500 text-white rounded text-xs py-2 px-3 hover:bg-sky-600', rest.className)}`}
+//         >{children}</button>
+//     )
+// }
+
 export default Button
--- src/pages/UsuariosList.jsx
+++ src/pages/UsuariosList.jsx
@@ -1,26 +1,8 @@
 import { useEffect, useState } from 'react'
-import { ButtonList, Page } from '../components'
+import { ButtonList, Page, Button } from '../components'
 import { supabase } from '../services/supabase'
 import Usuario from '../components/Usuario'
-import { Link } from 'react-router-dom'
 import { PiPencil } from 'react-icons/pi'
-import { twMerge } from 'tailwind-merge'
-
-function Button({ to, className, children, variant="normal", ...rest }){
-	const variants = {
-		normal: "bg-slate-300 border-slate-300 text-slate-600 hover:bg-slate-400",
-		primary: "bg-sky-500 border-sky-500 text-white",
-		outline: "border-slate-400",
-		success: "bg-emerald-500 border-emerald-500 text-white"
-	}
-	const classes = twMerge("border grid grid-flow-col-dense items-center rounded text-sm font-semibold py-2 px-2 outline-none", variants[variant], className)
-	//const classes = twMerge("grid grid-flow-col-dense items-center border bg-sky-500 text-white rounded text-xs py-2 px-3 hover:bg-sky-600", className)
-	if(to){
-		return <Link className={classes} to={to} {...rest}>{children}</Link>
-	} else {
-		return <button className={classes} {...rest}>{children}</button>
-	}
-}
 
 function UsuariosList() {
 	const [usuarios, setUsuarios] = useState([])
