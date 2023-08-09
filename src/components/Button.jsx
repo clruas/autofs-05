255700bfae816268e46b56c1cbf5ccd9ac605b4f
Cleverson <clruas@gmail.com>
2023-08-08 23:48:23 -0300

    Revisando layout do componente Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,10 @@ import { twMerge } from 'tailwind-merge'
 
 function Button({ to, className, children, variant="normal", ...rest }){
 	const variants = {
-		normal: "bg-slate-300 border-slate-300 text-slate-600 hover:bg-slate-400",
-		primary: "bg-sky-500 border-sky-500 text-white",
-		outline: "border-slate-400",
-		success: "bg-emerald-500 border-emerald-500 text-white"
+		normal: "bg-slate-300 border-slate-300 text-slate-600 hover:bg-slate-400 hover:border-slate-400",
+		primary: "bg-sky-500 border-sky-500 text-white hover:bg-sky-600",
+		outline: "border-slate-400 text-slate-500 hover:bg-slate-100",
+		success: "bg-emerald-500 border-emerald-500 text-white hover:bg-emerald-600"
 	}
 	const classes = twMerge("border grid grid-flow-col-dense items-center rounded text-sm font-semibold py-2 px-2 outline-none", variants[variant], className)
 	if(to){
